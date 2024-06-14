cce908b9972e01b241df52b6858017cf306ef1bc
sahanakh1985 <125340410+sahanakh1985@users.noreply.github.com>
2024-06-14 15:32:37 +0530

    Delete src directory

--- src/Login.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class Login {
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://online.actitime.com/skh/login.do");
-	}
-}
